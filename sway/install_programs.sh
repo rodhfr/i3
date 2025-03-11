8390eee4c8035b2aeffb88f5b88315c15a6a9160
Rodhfr <souzafrodolfo@gmail.com>
2025-03-11 11:56:09 -0300

    install script

---
+++ sway/install_programs.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo dnf update -y
+sudo dnf install git rust cargo nodejs-npm vim firefox python3-pip rofi htop nvim fastfetch -y
+sudo dnf copr enable pgdev/ghostty -y
+sudo dnf install ghostty -y
+pip3 install autotiling
