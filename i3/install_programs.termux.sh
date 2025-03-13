f675b6c996c871c349cabacacc6b12529997ff9b
rodolfo franca <souzafrodolfo@gmail.com>
2025-03-13 09:02:09 -0300

    i3 termux changes, feh, wallpaper to .config folder root

---
+++ i3/install_programs.termux.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#sudo dnf update -y
+#sudo dnf install git rust cargo nodejs-npm vim firefox python3-pip rofi htop nvim fastfetch -y
+pkg update -y
+pkg upgrade -y
+pkg install picom vim feh nvim rust nodejs nodejs firefox python3-pip rofi htop fastfetch -y
+#sudo dnf copr enable pgdev/ghostty -y
+#sudo dnf install ghostty -y
+pip3 install autotiling
